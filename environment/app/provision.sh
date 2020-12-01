3a9a46dd83391cd0ff5fc470ebd463171851b02a
farahmh <61236001+farahmh@users.noreply.github.com>
2020-12-01 10:40:46 +0000

    moved some files over, checked that gitignore works properly

---
+++ environment/app/provision.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+# install nginx
+sudo apt-get install nginx -y
+
+# copt config file into app files
+sudo cp /vagrant/nginx.conf /etc/nginx/
+
+# finally, restart the nginx service so the new config takes hold
+sudo service nginx restart
+# start the jsnode app
+cd /home/ubuntu/app
+
+echo "DB_HOST=192.168.10.150" >> ~/.bashrc
+
+source ~/.bashrc
+
+npm install
+
+pm2 start app.js
