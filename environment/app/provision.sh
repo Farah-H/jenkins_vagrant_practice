f78b45709ce4aa8e35a7a57ff8275bbb7924fd26
Farah-H <fhassan@spartaglobal.com>
2021-01-18 14:35:39 +0000

    Update provision.sh

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -15,7 +15,7 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # install pm2
-sudo npm install pm2 -g
+sudo npm install pm2 -y
 
 # install nginx
 sudo apt-get install nginx -y
